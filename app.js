bef3e57b68c7eb0cccbfefe8c3cc916e3a7f69b0
Bryson <73801883+B-HowellCMD@users.noreply.github.com>
2023-03-14 00:57:19 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,70 +1,17 @@
-const selectionButtons = document.querySelectorAll('[data-selection]');
-const finalColumn = document.querySelector('[data-final-column]')
-const computerScoreSpan = document.querySelector('[data-computer-score]')
-const yourScoreSpan = document.querySelector('[data-your-score]')
-// Array of objects (rock, paper, scissors)
-const SELECTIONS = [
-  {
-    name: 'rock',
-    emoji: '🤟',
-    beats: 'scissors'
-  },
-  {
-    name: 'paper',
-    emoji: '📝',
-    beats: 'rock'
-  },
-  {
-    name: 'scissors',
-    emoji: '✂️',
-    beats: 'paper'
-  }
-]
+// create a 16x16 grid of square divs
+let board = document.querySelector('.board');
+board.style.gridTemplateColumns = `repeat($size, 1fr)`;
+board.style.gridTemplateRows = `repeat($size, 1fr)`;
 
-// Logic for the buttons
-  selectionButtons.forEach(selectionButtons => {
-    selectionButtons.addEventListener('click', e => {
-        const selectionName = selectionButtons.dataset.selection
-        const selection = SELECTIONS.find(selection => selection.name === selectionName)
-        makeSelection(selection)
-    });
-});
-
-function makeSelection(selection) {
-  const computerSelection = randomSelection()
-  const yourWinner = isWinner(selection, computerSelection)
-  const computerWinner = isWinner(computerSelection, selection)
-  
-  // console.log(computerSelection)
-  
-  addSelectionResult(computerSelection, computerWinner)
-  addSelectionResult(selection, yourWinner)
-
-  // increments the score
-  if (yourWinner) incrementScore(yourScoreSpan)
-  if (computerWinner) incrementScore(computerScoreSpan)
+// creates our background grid
+for(let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+   square.style.backgroundColor = 'blue';
+   board.insertAdjacentElement('beforeend', square);
 }
 
-// increments the score
-function incrementScore(scoreSpan) {
-  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
-}
+populateBoard(16);
 
-// adds the selection to the DOM
-function addSelectionResult(selection, winner) {
-  const div = document.createElement('div')
-  div.innerText = selection.emoji
-  div.classList.add('result-selection')
-  if (winner) div.classList.add('winner')
-  finalColumn.after(div)
+function changeSize(input) {
+    populateBoard(input);
 }
-
-function isWinner(selection, opponentSelection) {
-  return selection.beats === opponentSelection.name
-}
-
-function randomSelection() {
-  const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-  return SELECTIONS[randomIndex]
-}
-
